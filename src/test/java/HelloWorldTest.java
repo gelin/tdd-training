51778f2cd29bd575d6d9ef9003b9093b90dd0380
Denis Nelubin <dnelubin@thumbtack.net>
2011-04-14 19:05:54 +0700

    Added test for Hello World.

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,3 +1,29 @@
+import static org.junit.Assert.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.Before;
+import org.junit.Test;
+
 public class HelloWorldTest {
 
+    ByteArrayOutputStream out;
+    
+    @Before
+    public void setUp() {
+        out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+    }
+    
+    @Test
+    public void testMain() {
+        HelloWorld.main(null);
+        assertEquals("Hello, TDD!\r\n", getOut());
+    }
+    
+    String getOut() {
+        return new String(out.toByteArray());
+    }
+    
 }
